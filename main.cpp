2861eb36a6842411e28f9a1bed3e737839f6229d
Aritra <aritra.03@proton.me>
2025-12-23 17:42:47 +0530

    Checked keystate using SDL_GetKeyboardState

--- main.cpp
+++ main.cpp
@@ -31,15 +31,19 @@ int main(int argc, char* argv[]){
             if(event.type == SDL_QUIT){
                 windowIsRunning = false;
             }
-            // if(event.type == SDL_MOUSEMOTION){
-            //     mouseMoveCount++;
-            //     std::cout << "Mouse has been moved for " << mouseMoveCount << " times\n";
-            // }
             if(event.type == SDL_KEYDOWN){
-                if(event.key.keysym.scancode == SDL_SCANCODE_B)
+                if(event.key.keysym.scancode == SDL_SCANCODE_B){
                     std::cout << "'B' key was pressed\n";
-                else
+                }
+                else{
                     std::cout << "A key was pressed but it is was B\n";
+                }
+
+                int numkeys;
+                const Uint8* state = SDL_GetKeyboardState(&numkeys);
+                std::cout << static_cast<int>(*state) << ", total keys: "<< numkeys << "\n";
+                state += 4; //returns 1 if key 'A' is pressed since 4th index is SDL_SCANCODE_A
+                std::cout << static_cast<int>(*state) << ", total keys: "<< numkeys << "\n";
             }
         }
     }
